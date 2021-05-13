1c3032908d2c9c5245fcab15dc01ce172d766ae6
unknown <jlarson129@outlook.com>
2021-05-13 08:48:56 -0400

    finished task 2

--- index.js
+++ index.js
@@ -76,8 +76,26 @@ class Airplane {
   */
   
  class Car {
-    
+   constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
   }
+  fill(gallons) {
+    this.tank += gallons;
+  };
+  drive(distance) {
+    if (this.tank < distance/this.milesPerGallon) {
+      this.odometer += this.tank*this.milesPerGallon;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer}`;
+    } else {
+      this.tank -= distance/this.milesPerGallon;
+      this.odometer += distance;
+    }
+  };
+}
   
   /*
     TASK 3
