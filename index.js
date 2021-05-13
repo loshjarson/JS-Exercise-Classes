4c2321b4902b4969a468face5e9602d04a49b15a
unknown <jlarson129@outlook.com>
2021-05-13 09:46:00 -0400

    finished task 6

--- index.js
+++ index.js
@@ -175,13 +175,13 @@ constructor(object) {
      }
      listSubjects(){
        return `Loving ${this.favSubjects}`
-     }
+     };
      PRAssignment(subject){
        return `${this.name} has submitted a PR for ${subject}`
-     }
+     };
      sprintChallenge(subject){
       return `${this.name} has begun sprint challenge on ${subject}`
-     }
+     };
  }
   
   /*
@@ -197,8 +197,18 @@ constructor(object) {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
+ class ProjectManager extends Instructor{
+   constructor(object){
+     super(object);
+     this.gradClassName = object.gradClassName;
+     this.favInstructor = object.favInstructor;
+   }
+   standUp(channel){
+     return `${this.name} announces to ${channel}, @channel standy times!`
+   }
+   debugsCode(student, subject) {
+     return `${this.name} debugs ${student.name}'s code on ${subject}`
+   }
  }
   /*
     STRETCH PROBLEM (no tests!)
